76255dcd3602bc87ca8ee6315ae8b97f820ed3f1
夏目 <1317981820@qq.com>
2022-08-05 22:44:36 +0800

    第一人称游戏
    角色移动

---
+++ Assets/Script/CameraLock.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 控制摄像机
+/// 玩家左右旋转控制实现镜头左右旋转
+/// 摄像机上下旋转控制镜头上下旋转
+/// </summary>
+public class CameraLock : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+
+    private float mouseX;
+    private float mouseY;
+
+    private float xRotation = 0f;
+    private float yRotation = 0f;
+    private void Update()
+    {
+        this.mouseX = Input.GetAxis("Mouse X") * this.mouseSensitivity * Time.deltaTime;
+        this.mouseY = Input.GetAxis("Mouse Y") * this.mouseSensitivity * Time.deltaTime;
+        xRotation += this.mouseX;
+        yRotation += this.mouseY;
+
+        Debug.LogError($"this.xRotation  = {this.xRotation },  this.yRotation = { this.yRotation}");
+        this.transform.localRotation = Quaternion.Euler(yRotation, this.mouseY,0f);
+        playerBody.Rotate(Vector3.up * xRotation);
+    }
+
+}
---
+++ Assets/Script/PlayerMovment.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 控制玩家移动
+/// </summary>
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovment : MonoBehaviour
+{
+    private CharacterController CharacterController;
+
+    public float Speed = 1f;
+    public Vector2 moveDirection;
+    private float mHorizontal;
+    private float mVertical;
+
+    private void Start()
+    {
+        if(this.CharacterController == null)
+        {
+            this.CharacterController = this.GetComponent<CharacterController>();
+        }
+
+    }
+
+    private void Update()
+    {
+        this.Move();
+    }
+
+    public void Move()
+    {
+       this.mHorizontal = Input.GetAxis("Horizontal");
+        this.mVertical = Input.GetAxis("Vertical");
+
+
+        this.moveDirection = this.Speed * (this.transform.right * this.mHorizontal + this.transform.forward * this.mVertical).normalized;
+        this.CharacterController.Move(this.moveDirection);
+        Debug.Log($"fHorizontal = {this.mHorizontal}, fVertical = {this.mVertical}");
+    }
+}
